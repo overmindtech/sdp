8bffe0693f41f0c0deb20e487c3023ced3437988
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2020-07-23 22:42:31 +0100

    Lots of docs and go generation

---
+++ build/protogen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+# Repo dir
+REPOPATH=$(dirname "$SCRIPTPATH")
+
+# Golang build
+GO_SDP_BUILD_PATH=${REPOPATH}/go/sdp
+mkdir -p $GO_SDP_BUILD_PATH
+
+protoc -I $REPOPATH --go_out $GO_SDP_BUILD_PATH items.proto 
