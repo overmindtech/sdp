c59511131bbf454abc733307f5921cd4a47a63b3
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2020-10-18 12:38:26 +0100

    Updated build

--- build/protogen.sh
+++ build/protogen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink "$0")
 # Absolute path this script is in, thus /home/user/bin
@@ -8,10 +10,16 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 REPOPATH=$(dirname "$SCRIPTPATH")
 
 # Golang build
-GO_SDP_BUILD_PATH=${REPOPATH}/go/sdp
+GO_SDP_FINAL_PATH=${REPOPATH}/go
+GO_SDP_BUILD_PATH=$(mktemp -d)
+
+# Compile
+protoc -I $REPOPATH --go_out $GO_SDP_BUILD_PATH *.proto
 
 # Clean slate
-rm -rf GO_SDP_BUILD_PATH
-mkdir -p $GO_SDP_BUILD_PATH
+rm -rf $GO_SDP_FINAL_PATH
+mkdir -p $GO_SDP_FINAL_PATH
 
-protoc -I $REPOPATH --go_out $GO_SDP_BUILD_PATH *.proto 
+# Move into place
+mv "$GO_SDP_BUILD_PATH/github.com/dylanratcliffe/sdp/go/sdp" "$GO_SDP_FINAL_PATH/"
+rm -rf $GO_SDP_BUILD_PATH
