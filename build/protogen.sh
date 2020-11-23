08b64cbe2ae335be64de7e87450e47d7cde8759c
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2020-11-23 20:31:53 +0000

    Added a few more functions

--- build/protogen.sh
+++ build/protogen.sh
@@ -2,24 +2,20 @@
 
 set -x
 
-# Absolute path to this script, e.g. /home/user/bin/foo.sh
-SCRIPT=$(readlink "$0")
-# Absolute path this script is in, thus /home/user/bin
-SCRIPTPATH=$(dirname "$SCRIPT")
-# Repo dir
-REPOPATH=$(dirname "$SCRIPTPATH")
+# Check that we are running from the root of the project
+[ ! -f go.mod ] && echo "go.mod does not exist. You need to run this script from the project root" && exit 1
 
 # Golang build
-GO_SDP_FINAL_PATH=${REPOPATH}/go
+GO_SDP_FINAL_PATH=go/sdp
 GO_SDP_BUILD_PATH=$(mktemp -d)
 
 # Compile
-protoc -I $REPOPATH --go_out $GO_SDP_BUILD_PATH *.proto
+protoc -I . --go_out $GO_SDP_BUILD_PATH *.proto
 
 # Clean slate
-rm -rf $GO_SDP_FINAL_PATH
+rm -f $GO_SDP_FINAL_PATH/*.pb.go
 mkdir -p $GO_SDP_FINAL_PATH
 
 # Move into place
-mv "$GO_SDP_BUILD_PATH/github.com/dylanratcliffe/sdp/go/sdp" "$GO_SDP_FINAL_PATH/"
+mv -fv $GO_SDP_BUILD_PATH/github.com/dylanratcliffe/sdp/go/sdp/* $GO_SDP_FINAL_PATH
 rm -rf $GO_SDP_BUILD_PATH
---
+++ go/sdp/methods.go
@@ -0,0 +1,46 @@
+package sdp
+
+import (
+	"fmt"
+
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+// UniqueAttributeValue return the value of whatever the Unique Attribute if
+// for this item. This will then be converted to a string and returned
+func (i *Item) UniqueAttributeValue() string {
+	var value interface{}
+	var err error
+
+	value, err = i.GetAttributes().Get(i.UniqueAttribute)
+
+	if err == nil {
+		return fmt.Sprint(value)
+	}
+
+	return ""
+}
+
+// Get Returns the value of a given attribute by name. If the attribute is
+// a nested hash, nested values can be referenced using dot notation e.g.
+// location.country
+func (a *ItemAttributes) Get(name string) (interface{}, error) {
+	var m map[string]interface{}
+
+	m = a.GetAttrStruct().AsMap()
+
+	if v, ok := m[name]; ok == true {
+		return v, nil
+	}
+
+	return "", fmt.Errorf("Attribute %v not found", name)
+}
+
+// ToAttributes Convers a map[string]interface{} to an ItemAttributes object
+func ToAttributes(m map[string]interface{}) ItemAttributes {
+	attrStruct, _ := structpb.NewStruct(m)
+
+	return ItemAttributes{
+		AttrStruct: attrStruct,
+	}
+}
