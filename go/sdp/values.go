bffda8441f86f2f6a902c5d1ce5b70e78819d56e
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2020-11-23 21:28:55 +0000

    Steamlining

--- go/sdp/values.go
+++ go/sdp/values.go
@@ -32,94 +32,94 @@ func ToValue(i interface{}) (*structpb.Value, error) {
 
 	if i == nil {
 		return structpb.NewNullValue(), nil
-	} else {
-		switch v.Kind() {
-		case reflect.Bool:
-			return structpb.NewBoolValue(v.Bool()), nil
-		case reflect.Int:
-			return structpb.NewNumberValue(float64(v.Int())), nil
-		case reflect.Int8:
-			return structpb.NewNumberValue(float64(v.Int())), nil
-		case reflect.Int16:
-			return structpb.NewNumberValue(float64(v.Int())), nil
-		case reflect.Int32:
-			return structpb.NewNumberValue(float64(v.Int())), nil
-		case reflect.Int64:
-			return structpb.NewNumberValue(float64(v.Int())), nil
-		case reflect.Uint:
-			return structpb.NewNumberValue(float64(v.Uint())), nil
-		case reflect.Uint8:
-			return structpb.NewNumberValue(float64(v.Uint())), nil
-		case reflect.Uint16:
-			return structpb.NewNumberValue(float64(v.Uint())), nil
-		case reflect.Uint32:
-			return structpb.NewNumberValue(float64(v.Uint())), nil
-		case reflect.Uint64:
-			return structpb.NewNumberValue(float64(v.Uint())), nil
-		case reflect.Float32:
-			return structpb.NewNumberValue(float64(v.Float())), nil
-		case reflect.Float64:
-			return structpb.NewNumberValue(float64(v.Float())), nil
-		case reflect.String:
-			return structpb.NewStringValue(v.String()), nil
-		case reflect.Array:
-			var list *structpb.ListValue
-			var value *structpb.Value
-			var err error
+	}
 
-			list, err = structpb.NewList(i.([]interface{}))
+	switch v.Kind() {
+	case reflect.Bool:
+		return structpb.NewBoolValue(v.Bool()), nil
+	case reflect.Int:
+		return structpb.NewNumberValue(float64(v.Int())), nil
+	case reflect.Int8:
+		return structpb.NewNumberValue(float64(v.Int())), nil
+	case reflect.Int16:
+		return structpb.NewNumberValue(float64(v.Int())), nil
+	case reflect.Int32:
+		return structpb.NewNumberValue(float64(v.Int())), nil
+	case reflect.Int64:
+		return structpb.NewNumberValue(float64(v.Int())), nil
+	case reflect.Uint:
+		return structpb.NewNumberValue(float64(v.Uint())), nil
+	case reflect.Uint8:
+		return structpb.NewNumberValue(float64(v.Uint())), nil
+	case reflect.Uint16:
+		return structpb.NewNumberValue(float64(v.Uint())), nil
+	case reflect.Uint32:
+		return structpb.NewNumberValue(float64(v.Uint())), nil
+	case reflect.Uint64:
+		return structpb.NewNumberValue(float64(v.Uint())), nil
+	case reflect.Float32:
+		return structpb.NewNumberValue(float64(v.Float())), nil
+	case reflect.Float64:
+		return structpb.NewNumberValue(float64(v.Float())), nil
+	case reflect.String:
+		return structpb.NewStringValue(v.String()), nil
+	case reflect.Array:
+		var list *structpb.ListValue
+		var value *structpb.Value
+		var err error
 
-			if err != nil {
-				return value, err
-			}
+		list, err = structpb.NewList(i.([]interface{}))
 
-			return structpb.NewListValue(list), nil
-		case reflect.Slice:
-			var list *structpb.ListValue
-			var value *structpb.Value
-			var err error
-
-			list, err = structpb.NewList(i.([]interface{}))
-
-			if err != nil {
-				return value, err
-			}
-
-			return structpb.NewListValue(list), nil
-		case reflect.Map:
-			var str *structpb.Struct
-			var s map[string]interface{}
-			var value *structpb.Value
-			var err error
-
-			// str, err = structpb.NewStruct()
-			s = make(map[string]interface{})
-
-			// Loop of the map
-			iter := v.MapRange()
-			for iter.Next() {
-				k := iter.Key()
-				v := iter.Value()
-
-				keyString := k.String()
-				value, err := ToValue(v.Interface())
-
-				if err != nil {
-					return &structpb.Value{}, err
-				}
-
-				s[keyString] = value
-			}
-
-			str, err = structpb.NewStruct(s)
-
-			if err != nil {
-				return value, err
-			}
-
-			return structpb.NewStructValue(str), nil
-		default:
-			return &structpb.Value{}, fmt.Errorf("Could not convert %v to protocol buffer value", v.Kind())
+		if err != nil {
+			return value, err
 		}
+
+		return structpb.NewListValue(list), nil
+	case reflect.Slice:
+		var list *structpb.ListValue
+		var value *structpb.Value
+		var err error
+
+		list, err = structpb.NewList(i.([]interface{}))
+
+		if err != nil {
+			return value, err
+		}
+
+		return structpb.NewListValue(list), nil
+	case reflect.Map:
+		var str *structpb.Struct
+		var s map[string]interface{}
+		var value *structpb.Value
+		var err error
+
+		// str, err = structpb.NewStruct()
+		s = make(map[string]interface{})
+
+		// Loop of the map
+		iter := v.MapRange()
+		for iter.Next() {
+			k := iter.Key()
+			v := iter.Value()
+
+			keyString := k.String()
+			value, err := ToValue(v.Interface())
+
+			if err != nil {
+				return &structpb.Value{}, err
+			}
+
+			s[keyString] = value
+		}
+
+		str, err = structpb.NewStruct(s)
+
+		if err != nil {
+			return value, err
+		}
+
+		return structpb.NewStructValue(str), nil
+	default:
+		return &structpb.Value{}, fmt.Errorf("Could not convert %v to protocol buffer value", v.Kind())
 	}
 }
