06886823aafd92ce6635506769af7579c90ccc84
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2021-01-16 17:49:22 +0000

    Added error conversion method

--- go/sdp/errors.go
+++ go/sdp/errors.go
@@ -1,6 +1,19 @@
 package sdp
 
 // Ensure that the ItemRequestError is seen as a valid error in golang
-func (e ItemRequestError) Error() string {
+func (e *ItemRequestError) Error() string {
 	return e.GetErrorString()
+}
+
+// NewItemRequestError converts a regular error to an ItemRequestError of type
+// OTHER. If the input error is already an ItemRequestError then it is preserved
+func NewItemRequestError(err error) *ItemRequestError {
+	if sdpErr, ok := err.(*ItemRequestError); ok {
+		return sdpErr
+	}
+
+	return &ItemRequestError{
+		ErrorType:   ItemRequestError_OTHER,
+		ErrorString: err.Error(),
+	}
 }
