2cf1e499521c5eb472bcc26682cc51a8cf796e0c
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2021-03-31 17:10:22 +0100

    Updated golang build

--- build/go.sh
+++ build/go.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# This build requires golang
+cd go
+
 set -x
 
 # Golang build
-GO_SDP_FINAL_PATH=go/sdp
+GO_SDP_FINAL_PATH=sdp
 GO_SDP_BUILD_PATH=$(mktemp -d)
 
 # Compile
-protoc -I . --go_out $GO_SDP_BUILD_PATH *.proto
+protoc -I ../ --go_out $GO_SDP_BUILD_PATH ../*.proto
 
 # Clean slate
 rm -f $GO_SDP_FINAL_PATH/*.pb.go
@@ -16,3 +19,5 @@ mkdir -p $GO_SDP_FINAL_PATH
 # Move into place
 mv -fv $GO_SDP_BUILD_PATH/github.com/dylanratcliffe/sdp/go/sdp/* $GO_SDP_FINAL_PATH
 rm -rf $GO_SDP_BUILD_PATH
+
+cd -
