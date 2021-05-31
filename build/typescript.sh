664102638fb8a79b9a7802b12edc8732b21b075a
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2021-05-31 18:57:01 +0100

    Removed all typescript

--- build/typescript.sh
+++ build/typescript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -x
+set -e
 
 # Requires:
 #
 # * NodeJS
-cd typescript
 
 # Install the plugins
 npm install
@@ -14,7 +14,7 @@ npm install
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 
 # Golang build
-SDP_FINAL_PATH=.
+SDP_FINAL_PATH=../sdp.js
 SDP_BUILD_PATH=$(mktemp -d)
 
 # Compile
@@ -23,25 +23,25 @@ protoc \
     --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" \
     --js_out="import_style=commonjs,binary:${SDP_BUILD_PATH}" \
     --ts_out="${SDP_BUILD_PATH}" \
-    ../*.proto
+    --proto_path . \
+    *.proto
 
 # Clean slate
 rm -f $SDP_FINAL_PATH/*_pb.d.ts
 rm -f $SDP_FINAL_PATH/*_pb.js
-
-mkdir -p $SDP_FINAL_PATH
+rm -rf $SDP_FINAL_PATH/google
 
 # Move into place
 mv -fv $SDP_BUILD_PATH/* $SDP_FINAL_PATH
 rm -rf $SDP_BUILD_PATH
 
 # Compile extra typescript
+cd $SDP_FINAL_PATH
 tsc
-
 cd -
 
 set +x
 
 echo ""
 echo "Compile complete. Run the following to publish:"
-echo "cd typescript && npm publish && cd -"
+echo "cd ../sdp.js && npm publish && cd -"
